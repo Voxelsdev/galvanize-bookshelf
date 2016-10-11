2c656aae3e1918e031d2debbf476553827fb531e
Tyler Miller <tylermillerhome@gmail.com>
2016-10-11 15:09:10 -0700

    Fixed Huruku

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/bookshelf_dev'
+    connection: 'postgres://localhost/bookshelf_dev',
   },
 
   test: {
@@ -11,5 +11,9 @@ module.exports = {
     connection: 'postgres://localhost/bookshelf_test'
   },
 
-  production: {}
+  production: {
+    client: 'pg',
+    connection: 'postgres://vdsnrlagjevoyy:AOKtw6oviA0rdyEWhLHNPP-bmO@ec2-54-243-58-188.compute-1.amazonaws.com:5432/d3nmfbrtrbm8g7',
+    pool: {min: 2, max: 7}
+  }
 };
